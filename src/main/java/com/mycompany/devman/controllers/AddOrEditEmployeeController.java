b8453fe8fe53aa4b761cd44198100f5747c205b6
Jakub Sierżęga <kuba3351@gmail.com>
2017-03-22 14:10:30 +0100

    new windows and button bindings

--- src/main/java/com/mycompany/devman/controllers/AddOrEditEmployeeController.java
+++ src/main/java/com/mycompany/devman/controllers/AddOrEditEmployeeController.java
@@ -7,7 +7,12 @@ package com.mycompany.devman.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +21,35 @@ import javafx.fxml.Initializable;
  */
 public class AddOrEditEmployeeController implements Initializable {
 
+    @FXML
+    Button cancel;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
     
+    private void close() {
+        Stage window = (Stage) cancel.getScene().getWindow();
+        window.close();
+    }
+    
+    public void onCancelButtonClick() {
+        close();
+    }
+    
+    public void onAddButtonClick() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Informacja");
+        alert.setHeaderText("Pracownik dodany!");
+        alert.setContentText("Tymczasowe hasło pracownika zostanie wysłane na jego adres e-mail.");
+        alert.showAndWait().ifPresent(rs -> {
+        if (rs == ButtonType.OK) {
+            close();
+        }
+      });
+    }   
 }
--- src/main/java/com/mycompany/devman/controllers/ManagerPanelController.java
+++ src/main/java/com/mycompany/devman/controllers/ManagerPanelController.java
@@ -3,11 +3,20 @@ package com.mycompany.devman.controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
+import javax.swing.event.HyperlinkEvent;
 
 /**
  * FXML Controller class
@@ -16,29 +25,67 @@ import javafx.stage.Stage;
  */
 public class ManagerPanelController implements Initializable {
 
+    @FXML
+    TabPane tabPanel;
+
+    @FXML
+    MenuBar menuBar;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        menuBar.getMenus().filtered(menu -> menu.getText().equals("Plik")).get(0).getItems().filtered(item -> item.getText().equals("Zamknij")).get(0).addEventHandler(EventType.ROOT, new EventHandler() {
+            @Override
+            public void handle(Event t) {
+                close();
+            }
+        });
+        menuBar.getMenus().filtered(menu -> menu.getText().equals("Pomoc")).get(0).getItems().filtered(item -> item.getText().equals("Informacje")).get(0).addEventHandler(EventType.ROOT, new EventHandler() {
+            @Override
+            public void handle(Event t) {
+                Stage infoWindow = new Stage();
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("/fxml/Info.fxml"));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+                Scene scene = new Scene(root);
+                scene.getStylesheets().add("/styles/register.css");
+
+                infoWindow.setTitle("DevMan - Informacje");
+                infoWindow.setResizable(false);
+                infoWindow.setScene(scene);
+                infoWindow.setX(20);
+                infoWindow.setY(20);
+                infoWindow.show();
+            }
+        });
     }
-    
+
+    private void close() {
+        Stage window = (Stage) tabPanel.getScene().getWindow();
+        window.close();
+    }
+
     public void onTasksInProgressClick() {
-        
+        tabPanel.getSelectionModel().select(tabPanel.getTabs().filtered(tab -> tab.getText().equals("Zadania")).get(0));
     }
-    
+
     public void onProjectsInProgressClick() {
-        
+        tabPanel.getSelectionModel().select(tabPanel.getTabs().filtered(tab -> tab.getText().equals("Projekty")).get(0));
     }
-    
+
     public void onEmployeeVerifyClick() throws IOException {
         Stage employeeVerifyWindow = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/EmployeeVerify.fxml"));
-        
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/register.css");
-        
+
         employeeVerifyWindow.setTitle("DevMan - Weryfikacja pracowników");
         employeeVerifyWindow.setResizable(false);
         employeeVerifyWindow.setScene(scene);
@@ -46,19 +93,49 @@ public class ManagerPanelController implements Initializable {
         employeeVerifyWindow.setY(20);
         employeeVerifyWindow.show();
     }
-    
+
     public void onLeaveVerifyClick() throws IOException {
         Stage leaveRequestVerifyWindow = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LeaveRequest.fxml"));
-        
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/register.css");
-        
+
         leaveRequestVerifyWindow.setTitle("DevMan - Weryfikacja wniosków o urlop");
         leaveRequestVerifyWindow.setResizable(false);
         leaveRequestVerifyWindow.setScene(scene);
         leaveRequestVerifyWindow.setX(20);
         leaveRequestVerifyWindow.setY(20);
         leaveRequestVerifyWindow.show();
+    }
+    
+    public void onAddEmployeeClick() throws IOException {
+        Stage employeeAddWindow = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/AddOrEditEmployee.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/register.css");
+
+        employeeAddWindow.setTitle("DevMan - Weryfikacja pracowników");
+        employeeAddWindow.setResizable(false);
+        employeeAddWindow.setScene(scene);
+        employeeAddWindow.setX(20);
+        employeeAddWindow.setY(20);
+        employeeAddWindow.show();
+    }
+    
+    public void onEditEmployeeClick() throws IOException {
+        Stage employeeAddWindow = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/AddOrEditEmployee.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/register.css");
+
+        employeeAddWindow.setTitle("DevMan - Weryfikacja pracowników");
+        employeeAddWindow.setResizable(false);
+        employeeAddWindow.setScene(scene);
+        employeeAddWindow.setX(20);
+        employeeAddWindow.setY(20);
+        employeeAddWindow.show();
     }
 }
