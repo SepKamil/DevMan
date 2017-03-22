cc83e34e3556436ad7333c5b2a268cc1cee4a1b9
Jakub Sierżęga <kuba3351@gmail.com>
2017-03-22 11:12:14 +0100

    Windows to buttons binding

--- src/main/java/com/mycompany/devman/controllers/ManagerPanelController.java
+++ src/main/java/com/mycompany/devman/controllers/ManagerPanelController.java
@@ -1,8 +1,13 @@
 package com.mycompany.devman.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,6 +22,43 @@ public class ManagerPanelController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
     
+    public void onTasksInProgressClick() {
+        
+    }
+    
+    public void onProjectsInProgressClick() {
+        
+    }
+    
+    public void onEmployeeVerifyClick() throws IOException {
+        Stage employeeVerifyWindow = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/EmployeeVerify.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/register.css");
+        
+        employeeVerifyWindow.setTitle("DevMan - Weryfikacja pracowników");
+        employeeVerifyWindow.setResizable(false);
+        employeeVerifyWindow.setScene(scene);
+        employeeVerifyWindow.setX(20);
+        employeeVerifyWindow.setY(20);
+        employeeVerifyWindow.show();
+    }
+    
+    public void onLeaveVerifyClick() throws IOException {
+        Stage leaveRequestVerifyWindow = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LeaveRequest.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/register.css");
+        
+        leaveRequestVerifyWindow.setTitle("DevMan - Weryfikacja wniosków o urlop");
+        leaveRequestVerifyWindow.setResizable(false);
+        leaveRequestVerifyWindow.setScene(scene);
+        leaveRequestVerifyWindow.setX(20);
+        leaveRequestVerifyWindow.setY(20);
+        leaveRequestVerifyWindow.show();
+    }
 }
