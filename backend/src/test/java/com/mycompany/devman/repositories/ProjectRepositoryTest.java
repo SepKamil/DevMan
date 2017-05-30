d1937ba23c444770fc40211403ee0e2755b75d53
Karolina <kjeraska@gmail.com>
2017-05-30 20:27:13 +0200

    test

--- backend/src/test/java/com/mycompany/devman/repositories/ProjectRepositoryTest.java
+++ backend/src/test/java/com/mycompany/devman/repositories/ProjectRepositoryTest.java
@@ -1,14 +1,18 @@
 package com.mycompany.devman.repositories;
 
 import com.mycompany.devman.BackendSetup;
-import com.mycompany.devman.domain.Project;
-import com.mycompany.devman.domain.User;
+import com.mycompany.devman.domain.*;
 import org.h2.engine.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.model.relational.Database;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.criteria.internal.expression.ParameterExpressionImpl;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
 import java.time.LocalDate;
 
 import static org.junit.Assert.*;
@@ -17,6 +21,25 @@ import static org.junit.Assert.*;
  * Created by bloczek on 17.05.2017.
  */
 public class ProjectRepositoryTest {
+    @Before
+    public void setup() {
+        SessionFactory sessionFactory = new Configuration().addAnnotatedClass(User.class)
+                .addAnnotatedClass(Leave.class)
+                .addAnnotatedClass(Message.class)
+                .addAnnotatedClass(Project.class)
+                .addAnnotatedClass(Task.class)
+                .addAnnotatedClass(Team.class)
+                .addAnnotatedClass(WorkTime.class)
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
+                .setProperty("hibernate.connection.driver_class", "org.h2.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:h2:mem:")
+                .setProperty("hibernate.hbm2ddl.auto", "update")
+                .buildSessionFactory();
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        BackendSetup.setSessionFactory(sessionFactory);
+        BackendSetup.setValidatorFactory(validatorFactory);
+    }
+
     @Test (expected = Exception.class)
     public void addProject() throws Exception {
         org.hibernate.Session session = BackendSetup.getDatabaseSession();
